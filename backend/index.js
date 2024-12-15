49ba69e8ae948189dd9f45a2dd120af95f5bcfa9
prajapatsachin7078 <sp639856@gmail.com>
2024-12-15 15:58:25 +0530

    updated api uri

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 const httpServer = createServer(app);
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://chat-app-git-main-sachin-prajapatis-projects.vercel.app',
     credentials: true
 }));
 app.use(json());
@@ -44,7 +44,7 @@ const io = new Server(httpServer, {
     pingInterval: 25000,             // Keep the ping interval
     pingTimeout: 60000,          // Keep the ping timeout
     cors: {
-        origin: 'http://localhost:5173',
+        origin: 'https://chat-app-git-main-sachin-prajapatis-projects.vercel.app',
         methods: ["GET", "POST"],
         credentials: true,
     }

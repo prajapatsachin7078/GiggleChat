897238fdec234fcd4541f45084531d460244ed9f
prajapatsachin7078 <sp639856@gmail.com>
2024-12-19 16:28:57 +0530

    one more git unexpected-text removed

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -2,32 +2,11 @@ import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge"
 import { format } from "date-fns";
 
-export const formatTimestamp = (createdAt) => {
-  const date = new Date(createdAt);
-
-  // If the message was sent today, show only the time (e.g., "12:30 PM")
-  if (format(date, "yyyy-MM-dd") === format(new Date(), "yyyy-MM-dd")) {
-    return format(date, "hh:mm a");
-  }
-
-  // Otherwise, display "Yesterday, hh:mm a" or the full date
-  const isYesterday =
-    format(date, "yyyy-MM-dd") ===
-    format(new Date(Date.now() - 86400000), "yyyy-MM-dd");
-  return isYesterday
-    ? `Yesterday, ${format(date, "hh:mm a")}`
-    : format(date, "MMM dd, hh:mm a");
-};
-
-
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
 import { format } from "date-fns";
 
-<<<<<<< HEAD
-export const API = "https://chitchat-j7mk.onrender.com";
-=======
 export const formatTimestamp = (createdAt) => {
   const date = new Date(createdAt);
 
@@ -44,4 +23,3 @@ export const formatTimestamp = (createdAt) => {
     ? `Yesterday, ${format(date, "hh:mm a")}`
     : format(date, "MMM dd, hh:mm a");
 };
->>>>>>> test
